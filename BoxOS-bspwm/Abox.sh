0f7421064a814ea0910463e13aad308dbf5bd07f
kjfreidhof <freidhofkyle@gmail.com>
2023-07-13 23:43:26 -0400

    Update to Abox.sh

--- BoxOS-bspwm/Abox.sh
+++ BoxOS-bspwm/Abox.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash 
 
-# set -e
+set -e
 
 req=("git" "vim")
 x=("xorg-server" "xorg-apps" "xorg-xinit")
@@ -8,7 +8,8 @@ bsp=("bspwm" "polybar" "sxhkd" "rofi" "kitty" "firefox" "nitrogen" "aria2" "pico
 audio=("pipewire" "pipewire-audio" "pipewire-alsa" "pipewire-pulse" "sof-firmware" "pavucontrol")
 blue=("bluez" "bluez-utils")
 fonts="ttf-fira-code"
-
+virt=("virt-manager" "qemu-desktop" "dnsmasq" "iptables-nft")
+flame="flameshot"
 
 xwm="chmod +x .config/bspwm/bspwmrc"
 xpol="chmod +x .config/polybar/launch.sh"
@@ -28,5 +29,9 @@ sudo pacman -S "${audio[@]}"
 sudo pacman -S "$font"
 
 sudo pacman -S "${blue[@]}"
+
+sudo pacman -S "${virt[@]}"
+
+sudo pacman -S "$flame"
 
 $xwm && $xpol && $xsdl && $conf && $bash && $xin 
