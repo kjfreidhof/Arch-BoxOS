4ecca6a979e8c8930f0e9fcf2ce37360b524e4cc
kjfreidhof <freidhofkyle@gmail.com>
2023-07-14 22:49:11 -0400

    BoxOS-sway has moved into alpha

--- BoxOS-sway/Abox.sh
+++ BoxOS-sway/Abox.sh
@@ -4,7 +4,7 @@ set -e
 
 req=("git" "vim" "polkit-gnome")
 x=("xorg-server" "xorg-apps" "xorg-xinit")
-sway=("sway" "rofi" "kitty" "firefox" "aria2" "gvfs" "pcmanfm")
+sway=("sway" "swaylock" "swayidle" "swaybg" "rofi" "kitty" "firefox" "aria2" "gvfs" "pcmanfm")
 audio=("pipewire" "pipewire-audio" "pipewire-alsa" "pipewire-pulse" "sof-firmware" "pavucontrol")
 blue=("bluez" "bluez-utils")
 fonts="ttf-fira-code"
